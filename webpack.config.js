13c6c499cb1d9a21f4bd8c6bbb3088070ff71064
Veaceslav Barbarii <slava.barbarii@gmail.com>
2023-08-06 11:58:04 +0300

    Made namings more meaningful

--- webpack.config.js
+++ webpack.config.js
@@ -2,16 +2,16 @@ import webpack from "webpack";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const currentFileName = fileURLToPath(import.meta.url);
+const currentDirectoryName = dirname(currentFileName);
 
 const config = {
     entry: {
-        "main": path.resolve(__dirname, "src", "index.js"),
+        "main": path.resolve(currentDirectoryName, "src", "index.js"),
     },
     output: {
         clean: true,
-        path: path.resolve(__dirname, "dist"),
+        path: path.resolve(currentDirectoryName, "dist"),
         filename: "[name].[contenthash].js"
     }
 };
