c04d38a3e59198c3f083dd8a74a48b2f72aca121
Veaceslav Barbarii <slava.barbarii@gmail.com>
2023-08-07 11:31:55 +0300

    created typings for images

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,17 @@
 import logger from "./components/logger.js";
 import greeter from "./components/greeter";
 
+import cocktails from "./assets/cocktails.svg";
+import aeronaut from "./assets/aeronaut-2016.png";
+import cat from "./assets/cat.jpg";
+import spiderMan from "./assets/spider-man.gif";
+
+
 logger.log("Hello World");
 
 greeter("Alex");
+
+logger.log(cocktails);
+logger.log(aeronaut);
+logger.log(cat);
+logger.log(spiderMan);
